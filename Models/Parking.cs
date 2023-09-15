cd7b31c49b98d62832d2daf56fbe340da05caddc
Jackson Moraes <jacksontadeu@hotmail.com>
2023-09-15 15:29:45 -0300

    Add: Refatoracao e melhoras de metodos

---
+++ Models/Parking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Models
+{
+    public class Parking
+    {
+        private decimal ValorInicial { get; set; }
+        private decimal ValorHora { get; set; }
+        private  Veiculo Veiculo { get; set; }
+
+        private List<Veiculo> veiculos = new List<Veiculo>();
+
+        public Parking(decimal valorInicial, decimal valorHora)
+        {
+            ValorInicial = valorInicial;
+            ValorHora = valorHora;
+        }
+        public void AdicionarVeiculo()
+        {
+            Console.Clear();
+            Console.Write("Digite a placa do veículo: ");
+            string placa = Console.ReadLine();
+            veiculos.Add(new Veiculo(placa));
+
+        }
+        public void RemoverVeiculo()
+        {
+            Console.Clear();
+            Console.Write("Digite a placa do veículo: ");
+            string placa = Console.ReadLine();
+            if (veiculos.Any(v => v.Placa.Equals(placa)))
+            {
+                Console.Write("Total de Horas: ");
+                int totalHoras = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Veículo {placa} removido com sucesso!!!");
+                Console.WriteLine($"Total a pagar: {CalcularValor(totalHoras).ToString("F2")}");
+                int index = veiculos.FindIndex(v=> v.Placa.Equals(placa));
+                veiculos.RemoveAt(index);
+                Console.ReadKey();
+               
+            }
+            else
+            {
+                Console.WriteLine("Veiculo não encontrado!!!");
+                Console.ReadKey();
+            }
+        }
+        public decimal CalcularValor(int totalHora)
+        {
+            decimal total = this.ValorInicial + (this.ValorHora * totalHora);
+            return total;
+        }
+        public void ListarVeiculos()
+        {
+            Console.Clear();
+            if (veiculos.Any())
+            {
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados!!!");
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,56 +8,30 @@ namespace Estacionamento
     {
         static void Main(string[] args)
         {
-            List<Veiculo> veiculos = new List<Veiculo>();
+            
             Console.Write("Valor de Entrada: ");
             decimal entrada = decimal.Parse(Console.ReadLine());
 
             Console.Write("Valor da Hora: ");
             decimal hora = decimal.Parse(Console.ReadLine());
+            Parking es = new Parking(entrada, hora);
             int opcao;
             do
             {
                 Menu();
-                Console.Write("Digite a opção ou 0 para sair:");
+                Console.Write("Digite a opção ou 0 para sair: ");
                 opcao = int.Parse(Console.ReadLine());
 
                 switch (opcao)
                 {
                     case 1:
-                        Console.Clear();
-                        Console.Write("Digite a placa do veículo: ");
-                        string placa = Console.ReadLine();
-                        veiculos.Add(new Veiculo(placa, entrada, hora));
+                        es.AdicionarVeiculo();
                         break;
                     case 2:
-                        Console.Clear();
-                        Console.Write("Digite a placa do veículo: ");
-                        string placaVeiculo = Console.ReadLine();
-                        for(int i = 0; i< veiculos.Count; i++)
-                        {
-                            if (veiculos[i].Placa.Equals(placaVeiculo))
-                            {
-                                Console.Write("Total de Horas: ");
-                                int totalHoras = int.Parse(Console.ReadLine());
-                                veiculos[i].CalcularValor(totalHoras);
-                                veiculos.Remove(veiculos[i]);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Veiculo não encontrado!!!");
-                                Console.ReadKey();
-                            }
-                        }
-                       
+                        es.RemoverVeiculo();
                         break;
                     case 3:
-                        Console.Clear();
-                        foreach (var veiculo in veiculos)
-                        {
-                            Console.WriteLine(veiculo.ToString());
-                        }
-                        Console.WriteLine("Pressione uma tecla para continuar!!!");
-                        Console.ReadKey();
+                        es.ListarVeiculos();
                         break;
                     case 0:
                         Console.WriteLine("Fechando o programa!!!");
